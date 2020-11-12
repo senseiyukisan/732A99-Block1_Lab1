7988cc7c41e2080e7b4871c083b0fbabd2667ac7
Yuki <yuki@Yukis-MacBook-Pro-2.local>
2020-11-12 21:03:58 +0100

    Assignment 3 finished until 4.6

--- Assignment2.R
+++ Assignment2.R
@@ -12,29 +12,25 @@ test = data[-id,]
 # Assume that Fat can be modeled as a linear regression in which absorbance characteristics (Channels) are used as features. 
 # Report the underlying probabilistic model, fit the linear regression to the training data and estimate the training and test errors. 
 
-# TODO Probabilistic Model in RMarkdown y^ = ß_0 + ß_1*x + ß_2*x + ... + ß_100*x + €
+# Done in RMarkdown
 
 # Fitting the linear regression model
 predictors <- paste("Channel", 1:100, sep="")
 fmla <- as.formula(paste("Fat ~ ", paste(predictors, collapse= "+")))
 
-fit = lm(fmla, data=data)
+fit = lm(fmla, data=train)
 summary(fit)
 
 # Estimating training and test errors
-yhat = predict(fit, train)
-train_mse = mean((train$Fat-yhat)^2)
-train_mae = mean(abs(train$Fat-yhat))
-train_rmse = sqrt(mean((train$Fat-yhat)^2))
+train_predictions_lm = predict(fit, train)
+train_mse = mean((train$Fat-train_predictions_lm)^2)
+train_rmse = sqrt(mean((train$Fat-train_predictions_lm)^2))
 
-y0hat = predict(fit, test)
-test_mse = mean((test$Fat-y0hat)^2)
-test_mae = mean(abs(test$Fat-y0hat))
-test_rmse = sqrt(mean((test$Fat-y0hat)^2))
+test_predictions_lm = predict(fit, test)
+test_mse = mean((test$Fat-test_predictions_lm)^2)
+test_rmse = sqrt(mean((test$Fat-test_predictions_lm)^2))
 
-cat("Train error\nMSE: ", train_mse, "\nMAE: ", train_mae, "\nRMSE: ", train_rmse, "\n\nTest error\nMSE: ", test_mse, "\nMAE: ", test_mae, "\nRMSE: ", test_rmse)
-
-fitted <- predict(fit, interval = "confidence")
+cat("Train error\nMSE: ", train_mse, "\nRMSE: ", train_rmse, "\n\nTest error\nMSE: ", test_mse, "\nRMSE: ", test_rmse)
 
 # Comment on the quality of fit and prediction and therefore on the quality of model.
 
@@ -43,9 +39,9 @@ fitted <- predict(fit, interval = "confidence")
 # Therefore, our model seems to be overfitting on the train data.
 
 # 2)
-# TODO Assume now that Fat can be modeled as a LASSO regression in which all Channels are used as features.
+# Assume now that Fat can be modeled as a LASSO regression in which all Channels are used as features.
 # Report the objective function that should be optimized in this scenario.
-# https://en.wikipedia.org/wiki/Lasso_(statistics) y_i = Fat_i, x_i = Channel_i
+# Done in RMarkdown
 
 # 3)
 
@@ -55,21 +51,26 @@ library(ggplot2)
 
 # Fit the LASSO regression model to the training data. 
 # [,-1] to remove the 'Sample' column
-x = as.matrix(scale(model.matrix(fmla, train)[,-1]))
-y = scale(train$Fat)
+covariates = scale(train[,2:101])
+response = scale(train$Fat)
 
 # alpha=1 to choose 'Lasso'
-model_lasso = glmnet(x, y, alpha=1, family="gaussian")
+model_lasso = glmnet(as.matrix(covariates), response, alpha=1, family="gaussian")
 
-# TODO Present a plot illustrating how the regression coefficients depend on the log of penalty factor (log 𝜆) and interpret this plot.
+# Present a plot illustrating how the regression coefficients depend on the log of penalty factor (log 𝜆) and interpret this plot.
 plot(model_lasso, xvar="lambda", label=T)
+# This plot shows the relationship between log(lambda) and value of coefficients in the lasso model. We can see that a bigger 
+# log(lambda) evaluates to less non-zero coefficients. At log(lambda) = -8 most of the variables are non-zero. At this point
+# we get the same coefficient values as for OLS. For each coefficient we can also see the influence (positive or negative)
+# on our prediction depending on different log(lambda) values.
 
-# TODO What value of the penalty factor can be chosen if we want to select a model with only three features?
+
+# What value of the penalty factor can be chosen if we want to select a model with only three features?
 # To solve this question we check the plot again and look for the lambda value that only has 3 nonzero coefficients. 
-# The first column 'Df' shows the number of non-zero coefficients. Therefore, we look for Df=3 and detect a penalty factor of log(0.06773).
+# We see that the coefficient with the label '40' goes to 0 at around Log Lambda = -2.8. We only have 3 non-zero coefficients left at that point.
 
 # 4) 
-# TODO Present a plot of how degrees of freedom depend on the penalty parameter. Is the observed trend expected?
+# Present a plot of how degrees of freedom depend on the penalty parameter. Is the observed trend expected?
 lambda = model_lasso$lambda
 df = model_lasso$df
 ggplot(data=NULL, aes(lambda, df)) + geom_point(color="red")
@@ -84,28 +85,55 @@ ggplot(data=NULL, aes(lambda, df)) + geom_point(color="red")
 # 5)
 # Repeat step 3 but fit Ridge instead of the LASSO regression and compare the plots from steps 3 and 5. Conclusions?
 
-# alpha=0 to choose 'Lasso'
-model_ridge = glmnet(x, y, alpha=0, family="gaussian")
+# alpha=0 to choose 'Ridge'
+model_ridge = glmnet(as.matrix(covariates), response, alpha=0, family="gaussian")
 plot(model_ridge, xvar="lambda", label=T)
+
+# Both Ridge and LASSO regression are shrinkage methods that try to reduce the complexity
+# of a model by shrinking or eliminating (LASSO only) its coefficients. For the Ridge regression
+# the number of coefficients stays the same regardless of how big log(lambda) gets.
+# For LASSO regression the number of coefficients decreases with increasing value of log(lambda).
+# Looking at the LASSO plot it is easier to determine which coefficients are the most
+# significant compared to looking at the Ridge plot.
 
 # 6)
 
 # Use cross-validation to compute the optimal LASSO model. 
-cv_lasso = cv.glmnet(x, y, alpha=1, family="gaussian", lambda=seq(0,1,0.001))
-
-# TODO Present a plot showing the dependence of the CV score on log𝜆and comment how the CV score changes with log𝜆. 
+cv_lasso = cv.glmnet(as.matrix(covariates), response, alpha=1, family="gaussian", lambda=seq(0,1,0.001))
+# Present a plot showing the dependence of the CV score on log𝜆and comment how the CV score changes with log𝜆. 
 plot(cv_lasso)
-print(cv_lasso)
-# 
+# We can see a correlation between increasing log lambda and increasing MSE. Up until a log lambda value of around -4 we observe a 
+# flat rise of MSE. Afterwards the MSE grows very steeply until it reaches a plateau at around -2.5. From that point until 
+# we observe a slow rise again until we reach log(lambda) = -1. From this point onward the MSE stays at its maximum since 
+# there are no non-zero coefficients left. 
 
 # Report the optimal 𝜆and how many variables were chosen in this model. 
-cat("The optimal 𝜆: ", cv_lasso$lambda.min, "\nNumber of variables choosen: ", length(coef(cv_lasso, s="lambda.min")))
+optimal_lambda = cv_lasso$lambda.min
+cat("The optimal 𝜆: ", optimal_lambda, "\nNumber of variables choosen: ", length(coef(cv_lasso, s="lambda.min")))
 
 # TODO Comment whether the selected 𝜆 value is statistically significantly better than log𝜆= −2. 
 
+# We are comparing the cv-score for our optimal lambda given by lambda.min with the cv-score for log(lambda) = -2.
+
+# transform lambda
+lambda_to_compare = exp(-2)
+
+# We check the cvm for lambda_to_compare and get a value of 0.81816120. Compared to our optimal lambda=0 with cv-score
+# of 0.06060929 we calculate 
+
+cv_lasso$cvm
+cv_lasso$lambda
+print(cv_lasso)
+
 # TODO Finally, create a scatter plot of the original test versus predicted test values for the model corresponding to optimal lambda 
 # and comment whether the model predictions are good.
+test_x = as.matrix(model.matrix(fmla, test)[,-1])
+test_y = scale(test$Fat)
 
+lasso_model <- glmnet(as.matrix(covariates), response, alpha = 1, lambda=optimal_lambda)
+test_predictions_lasso <- predict(lasso_model, s=optimal_lambda, newx=test_x, type="response")
+
+test_mse_lasso = mean((test_y-test_predictions_lasso)^2)
 
 # 7)
 # Use the feature values from test data (the portion of test data with Channel columns) and the optimal LASSO model from step 6 to generate new target values. 
