3741bad26c3cc1cbf7b73021cf07de0c166b1a54
Mowniesh Asokan <71217867+mowas455@users.noreply.github.com>
2020-11-14 04:17:24 +0530

    changes in model

--- Assignment1.R
+++ Assignment1.R
@@ -2,6 +2,7 @@ setwd("C:/Users/Mounish/Desktop/ML/lab1")
 dataset = read.csv("C:/Users/Mounish/Desktop/ML/Lab1/optdigits.csv")
 dim(dataset)
 
+
 set.seed(12345)
 sample_train<- sample(seq_len(nrow(dataset)), size = floor(0.50*nrow(dataset)))
 sample_valid<- sample(seq_len(nrow(dataset)), size = floor(0.25*nrow(dataset)))
@@ -15,20 +16,53 @@ dim(train)
 dim(test)
 dim(validation)
 
+## Data is splitted into training, tests and validation 
 
 #Model kknn
 
+
 library(kknn)
 
-knn.fit <- train.kknn(X0.26~., train, kmax = 30,
+knn.fit <- kknn(as.factor(X0.26)~., train=train,test=train ,k = 30,
                   kernel = "rectangular")
 
-pred.knn <- predict(knn.fit, train)
-pred.knn_v <- predict(knn.fit, validation)
-pred.knn_t <- predict(knn.fit, test)
+knn.fit_v <- kknn(as.factor(X0.26)~., train=validation,test=validation,k = 30,
+                  kernel = "rectangular")
 
-plot(pred.knn)
-table(pred.knn)
+knn.fit_t <- kknn(as.factor(X0.26)~., train=test,test=test,k = 30,
+                kernel = "rectangular")
+
+pred.knn <- fitted(knn.fit)
+pred.knn_v <- fitted(knn.fit_v)
+pred.knn_t <- fitted(knn.fit_t)
+
+
+## Model is worked and trained by using the training data 
+
+
+
+#Accuracy  of the model while using k value=30
+ 
+cm=as.matrix(table(actual=validation$X0.26, Predicted = pred.knn_v))
+
+accuracy=sum(diag(cm))/length(validation$X0.26)
+
+accuracy
+
+
+#
+calc_class_err = function(actual, predicted) {
+  mean(actual != predicted)
+}
+calc_class_err(validation$X0.26,pred.knn_v)
+
+
+# probability
+v<-as.data.frame(knn.fit$prob)
+
+dim(v)
+
+v[,9]
 
 #Confusion Matrix
 
@@ -51,44 +85,59 @@ missclassrate(train$X0.26,as.factor(pred.knn)) # for training data
 
 missclassrate(test$X0.26,as.factor(pred.knn_t)) # for test data
 
-?diag
-
-
-summary(pred.knn)
-
-table(train$X0.26)
-
-
+# Accuracy of the data
 acc=function(x,y)
 {
   n=length(x)
   ac=sum(diag(table(x,y)))/n
   return(ac)
 }
-acc(train$X0.26,as.factor(pred.knn))
+acc(test$X0.26,as.factor(pred.knn_t))
  
 
 
 
-####
-#accuracy_check=function(x)
-#{                    
+
+#Find the optimal K value for KNN model for this dataset.(k=1 to 30)                     
 
 k=1
 k.optm=c() 
-for (i in 1:30)
-{ 
-  knn.fit <- train.kknn(X0.26~., train, kmax = i,kernel = "rectangular")
+y.optm=c()
+for (i in 1:30){ 
+  knn.fit <-kknn(as.factor(X0.26)~., train=train,test=train, k = i,kernel = "rectangular")
   
-  ypred = predict(knn.fit,newdata = train)
+  knn.fit_v <-kknn(as.factor(X0.26)~., train=validation,test=validation, k = i,kernel = "rectangular")
+  
+  ypred = fitted(knn.fit)
+  
+  vpred = fitted(knn.fit_v)
   
   k.optm[i] = 1-(sum(diag(as.matrix(table(Actual = train$X0.26, Predicted = ypred))))/nrow(train))
   
+  y.optm[i] = 1-(sum(diag(as.matrix(table(Actual = validation$X0.26, Predicted = vpred))))/nrow(validation))
   
 }
 k.optm
 
-#}
+y.optm
 
-accuracy_check(1:30)
-plot(n)
+ ####
+library(ggplot2)
+xValue <- 1:30
+yValue <- y.optm,k.optm,
+data <- data.frame(xValue,yValue)
+ ggplot(data, aes(x=xValue, y=yValue)) +
+  geom_line()
+
+
+### Cross Entropy
+ 
+ cross.entropy <- function(p, phat){
+   x <- 0
+   for (i in 1:length(p)){
+         
+     x <- x + (p[i] * log(phat[i]))
+   }
+   return(-x)
+ }
+cross.entropy(as.factor(validation$X0.26),pred.knn_v)
